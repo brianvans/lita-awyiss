b0cfe2a20f901082d4cc6d7b5f4624ef1bd6fa0e
brianvans <brianvans@gmail.com>
2016-02-05 23:40:42 -0700

    add two test assertions

--- spec/lita/handlers/awyiss_spec.rb
+++ spec/lita/handlers/awyiss_spec.rb
@@ -1,4 +1,19 @@
 require "spec_helper"
 
 describe Lita::Handlers::Awyiss, lita_handler: true do
+  before do
+    stub_request(:post, "http://awyisser.com/api/generator").
+      with(:body => {"phrase"=>"lita plugin"}).
+      to_return(:status => 200, :body => '{"imgur":"http://imgur.com/cuaRe1S","link":"http://i.imgur.com/cuaRe1S.png"}')
+  end
+
+  it 'should reply with the png link' do
+    send_command("awyiss lita plugin")
+    expect(replies.last).to eq('http://i.imgur.com/cuaRe1S.png')
+  end
+
+  it 'should reply with the png link if there\'s two w characters' do
+    send_command("awwyiss lita plugin")
+    expect(replies.last).to eq('http://i.imgur.com/cuaRe1S.png')
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require "lita-awyiss"
 require "lita/rspec"
+require 'webmock/rspec'
 
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
